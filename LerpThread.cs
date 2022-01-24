0a1e26c72aacf05f4bae81b19bbfe40edbaf4740
WIjarn Bangmo <cinowacs@gmail.com>
2022-01-24 12:53:43 +0700

    [update] extension

--- LerpThread.cs
+++ LerpThread.cs
@@ -3,22 +3,53 @@ using UniRx;
 
 namespace Modules.Utilities
 {
-	internal static class LerpThread
-	{
-		//--------------------------------------------------------------------------------------------------------------
+    internal static class LerpThread
+    {
+        //--------------------------------------------------------------------------------------------------------------
 
-		public static IDisposable Execute(int _miliseconds, Action<long> _onNext, Action _onComplete = null)
-		{
-			return Observable
-				.EveryUpdate()
-				.Take(TimeSpan.FromMilliseconds(_miliseconds))
-				.Subscribe
-				(
-					_onNext,
-					_onComplete
-				);
-		}
-		
-		//--------------------------------------------------------------------------------------------------------------
-	}
+        public static IDisposable Execute(int _miliseconds, Action<long> _onNext, Action _onComplete = null)
+        {
+            return Observable
+                .EveryUpdate()
+                .Take(TimeSpan.FromMilliseconds(_miliseconds))
+                .Subscribe
+                (
+                    _onNext,
+                    _onComplete
+                );
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static IObservable<float> FloatLerp(int _milliseconds, float _start, float _target, Easing.Ease _ease = Easing.Ease.EaseInOutQuad)
+        {
+
+            return Observable.Create<float>(_observer =>
+            {
+                var progress = 0f;
+                IDisposable disposable = LerpThread
+                    .Execute(
+                        _milliseconds,
+                        _count =>
+                        {
+                            progress += UnityEngine.Time.deltaTime / (_milliseconds * GlobalConstant.MILLISECONDS_TO_SECONDS);
+                            var valueTarget = EasingFormula.EasingFloat(_ease, _start, _target, progress);
+                            _observer.OnNext(valueTarget);
+
+                        },
+                        () =>
+                        {
+
+                            _observer.OnNext(_target);
+                            _observer.OnCompleted();
+
+                        }
+                    );
+
+                return Disposable.Create(() => disposable?.Dispose());
+            });
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+    }
 }
