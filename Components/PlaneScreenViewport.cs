3758f13c05a6d078e4082523eaf754015905b23a
Draft Sama <draft.sama@gmail.com>
2024-01-23 16:58:03 +0700

    [update] add option setting in PlaneScreenViewport

--- Components/PlaneScreenViewport.cs
+++ Components/PlaneScreenViewport.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class PlaneScreenViewport : MonoBehaviour
     [SerializeField] private Camera m_Camera;
 
     [SerializeField] private float m_Distance = 1.0f;
+    [SerializeField] private float m_AdditionalScalePer = 0f;
+
     [SerializeField] private bool m_UpdateAlways = true;
 
     private Transform _Tr;
@@ -27,8 +30,18 @@ public class PlaneScreenViewport : MonoBehaviour
         //Calculate the size of the screen plane based on the camera's field of view and the distance to the screen plane
         var size = 2.0f * m_Distance * Mathf.Tan(m_Camera.fieldOfView * 0.5f * Mathf.Deg2Rad);
 
+
+        var width = size * m_Camera.aspect;
+        var height = size;
+
+        if (m_AdditionalScalePer < 0) m_AdditionalScalePer = 0;
+        width += (width * m_AdditionalScalePer) / 100f;
+        height += (height * m_AdditionalScalePer) / 100f;
+
+
+
         // Set the scale of the screen plane to match the calculated size and aspect ratio
-        _Tr.localScale = new Vector3(size * m_Camera.aspect, size, 1.0f);
+        _Tr.localScale = new Vector3(width, height, 1.0f);
 
 
         _Tr.position = m_Camera.transform.position + m_Camera.transform.forward * m_Distance;
@@ -41,8 +54,9 @@ public class PlaneScreenViewport : MonoBehaviour
     }
 
 
-    private void Update() {
-        if(m_UpdateAlways)UpdatePlaneScreen();
+    private void Update()
+    {
+        if (m_UpdateAlways) UpdatePlaneScreen();
     }
 
 
