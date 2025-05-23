542e4e54e085f1b97df16c58cdb8b713133ed12c
Draft Sama <draft.sama@gmail.com>
2025-05-23 10:10:13 +0700

    [add] dropdown field on custom inspector

--- Editor/CustomAttributeDrawer.cs
+++ Editor/CustomAttributeDrawer.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Modules.Utilities;
 using UnityEditor;
@@ -18,7 +21,7 @@ namespace Modules.Utilities.Editor
         }
 
     }
-    
+
     [CustomEditor(typeof(MonoBehaviour), true)]
     public class ButtonEditor : UnityEditor.Editor
     {
@@ -44,6 +47,61 @@ namespace Modules.Utilities.Editor
             }
         }
     }
-    
+
+    [CustomPropertyDrawer(typeof(DropdownFieldAttribute))]
+    public class DropdownFieldAttributeDrawer : PropertyDrawer
+    {
+
+        int selectedIndex = 0;
+       
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                // Create a dropdown with the predefined strings
+                // Debug.Log("m_Options: " + m_Options.Length);
+
+                Dictionary<string,string> fieldOptions = ((DropdownFieldAttribute)attribute).m_Options;
+
+                string currentValue = property.stringValue;
+
+                // Find the index of the current value in the options
+                foreach (var kvp in fieldOptions)
+                {
+                    if (kvp.Value == currentValue)
+                    {
+                        selectedIndex = Array.IndexOf(fieldOptions.Keys.ToArray(), kvp.Key);
+                        break;
+                    }
+                }
+
+                // Create a dropdown with the predefined strings
+                GUIContent[] options = new GUIContent[fieldOptions.Count];
+                int i = 0;
+                foreach (var kvp in fieldOptions)
+                {
+                    options[i] = new GUIContent()
+                    {
+                        text = kvp.Key,
+                        tooltip = $"\"{kvp.Value}\""
+                    };
+                    i++;
+                }
+                label.tooltip = $"Value: \"{fieldOptions.Values.ElementAt(selectedIndex)}\"";
+                selectedIndex = EditorGUI.Popup(position, label, selectedIndex, options);
+
+                // Set the property value to the selected option
+                property.stringValue = fieldOptions.Values.ElementAt(selectedIndex);
+
+
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+        }
+    }
+
 
 }
--- Runtime/Entities/CustomAttributes.cs
+++ Runtime/Entities/CustomAttributes.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace Modules.Utilities
@@ -15,6 +17,36 @@ namespace Modules.Utilities
     {
     }
 
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class DropdownFieldAttribute : PropertyAttribute
+    {
+        public Dictionary<string, string> m_Options;
+
+
+        public DropdownFieldAttribute(Type type)
+        {
+            m_Options = GetStringConstants(type);
+
+        }
+
+         private Dictionary<string, string> GetStringConstants(Type type)
+        {
+            // Get all the fields in the class
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Static);
+            Dictionary<string, string> constants = new Dictionary<string, string>();
+
+            foreach (var field in fields)
+            {
+                // Check if the field is a string constant
+                if (field.FieldType == typeof(string) && field.IsLiteral && !field.IsInitOnly)
+                {
+                    constants.Add(field.Name, (string)field.GetValue(null));
+                }
+            }
+
+            return constants;
+        }
+    }
 
 
 
